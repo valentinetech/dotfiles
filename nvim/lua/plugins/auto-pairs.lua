590f93b501a9aac1c8ef1a62ae762eef2c96380f
Valentinas Tauškėla <valentinas.tauskela@hostinger.com>
2026-03-06 15:23:04 +0200

    fix: autopairs not being able to delete only one brace

--- nvim/lua/plugins/auto-pairs.lua
+++ nvim/lua/plugins/auto-pairs.lua
@@ -8,6 +8,7 @@ return {
     local autopairs = require("nvim-autopairs")
     
     autopairs.setup({
+      map_bs = false,
       check_ts = true,
       ts_config = {
         lua = { "string" },
